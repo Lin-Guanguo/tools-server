2fae8c5ddcc7bb3d748b61935a412af2b7635db4
linguanguo <linguanguo@bytedance.com>
2022-12-04 16:36:22 +0800

    Add vars and function

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 
 use async_process::{Command, Stdio};
 use futures::AsyncWriteExt;
+use regex::{Captures, Regex};
 use warp::{hyper::body::Bytes, Filter};
 
 #[tokio::main]
@@ -19,38 +20,67 @@ async fn execute(
     app: String,
     query: HashMap<String, String>,
     headers: warp::hyper::HeaderMap,
-    mut body: Bytes,
+    body: Bytes,
 ) -> ExecuteReply {
-    let mut parse_header_args = |h: &str| {
+    let parse_header_args = |h: &str, vars: &HashMap<String, String>| {
         headers
             .get_all(h)
-            .into_iter()
-            .flat_map(|h| h.to_str().unwrap().split(','))
-            .flat_map(|s| shell_words::split(s).unwrap())
-            .map(|s| match s.as_str() {
-                "$body" => {
-                    let (head, tail) = split_blank_line(body.clone());
-                    body = tail;
-                    String::from_utf8_lossy(&head).to_string()
-                }
-                key if s.starts_with('$') => query.get(&key[1..]).cloned().unwrap_or(s),
-                _ => s,
-            })
+            .iter()
+            .flat_map(|s| s.to_str().unwrap().split(","))
+            .map(|s| replace_variable(&vars, s))
+            .flat_map(|s| shell_words::split(&s).unwrap())
             .collect::<Vec<_>>()
     };
-    let args = parse_header_args("args");
-    let opts = parse_header_args("opts");
+
+    // query vars
+    let mut vars = query;
+
+    // header vars
+    let header_vars = parse_header_args("vars", &vars);
+    let mut body = body;
+    header_vars
+        .iter()
+        .map(|s| {
+            let mut i = s.split("=");
+            (i.next(), i.next())
+        })
+        .for_each(|kv| match kv {
+            // TODO: approve body vars
+            (Some(k), Some("body")) => {
+                let (head, tail) = split_blank_line(body.clone());
+                body = tail;
+                let v = String::from_utf8_lossy(&head);
+                let v = replace_variable(&vars, &v);
+                vars.insert(k.to_string(), v.to_string());
+            }
+            (Some(k), Some(v)) => {
+                vars.insert(k.to_string(), v.to_string());
+            }
+            _ => {}
+        });
+
+    let args = parse_header_args("args", &vars);
+    let opts = parse_header_args("opts", &vars);
+
+    // parse-body
+    if let Some(_) = opts.iter().find(|s| *s == "parse-body") {
+        let body2 = String::from_utf8_lossy(&body);
+        let body2 = replace_variable(&vars, &body2);
+        let body2 = Bytes::from(body2.to_string());
+        body = body2
+    }
 
     println!(
-        " === accpet command === \napp: {}\nquery: {:?}\nheaders: {:?} \nbody: {:?}\nargs: {:?}\nopts: {:?}",
+        " === accpet command === \napp: {}\nvars: {:?}\nheaders: {:?} \nbody: {:?}\nargs: {:?}\nopts: {:?}",
         app,
-        query,
+        vars,
         headers,
         String::from_utf8_lossy(&body),
         args,
         opts
     );
 
+    // start execute
     let start = std::time::Instant::now();
     let mut child = Command::new(format!("./tools/{}", app))
         .args(args)
@@ -69,6 +99,7 @@ async fn execute(
     let out = child.output().await.unwrap();
     let end = std::time::Instant::now();
 
+    // reply
     if let Some(_) = opts.iter().find(|s| *s == "stdout") {
         ExecuteReply::Binary(out.stdout)
     } else if let Some(_) = opts.iter().find(|s| *s == "stderr") {
@@ -107,3 +138,47 @@ fn split_blank_line(mut input: Bytes) -> (Bytes, Bytes) {
         (input, Bytes::new())
     }
 }
+
+fn replace_variable<'a>(
+    vars: &HashMap<String, String>,
+    input: &'a str,
+) -> std::borrow::Cow<'a, str> {
+    lazy_static::lazy_static!(
+        static ref VARIABLE_PAT: Regex = Regex::new(
+            r#"(?x)
+                ( \$(?P<var1>[0-9A-Za-z_]+) ) |
+                ( \$\{\s*(?P<var2>[0-9A-Za-z_]+)\s*\} ) |
+                ( \$\{\s* (?P<fn>[0-9A-Za-z_]+) \( \s*(?P<input>[^\)]*)\s* \) \s*\} )
+            "#,
+        ).unwrap();
+    );
+
+    VARIABLE_PAT.replace_all(input, |cap: &Captures| {
+        if let Some(var) = cap.name("var1") {
+            vars.get(var.as_str())
+                .cloned()
+                .unwrap_or_else(|| format!("${{{}}}", var.as_str()))
+        } else if let Some(var) = cap.name("var2") {
+            vars.get(var.as_str())
+                .cloned()
+                .unwrap_or_else(|| format!("${{{}}}", var.as_str()))
+        } else if let Some(fname) = cap.name("fn") {
+            let input = cap.name("input").unwrap();
+            let input = replace_variable(&vars, input.as_str());
+            let input = input.as_ref();
+            match fname.as_str() {
+                "base64" => base64(input),
+                fname => format!("${{{}{})}}", fname, input),
+            }
+        } else {
+            panic!("inner error")
+        }
+    })
+}
+
+fn base64(input: &str) -> String {
+    base64::encode(input)
+}
+
+#[test]
+fn test() {}
